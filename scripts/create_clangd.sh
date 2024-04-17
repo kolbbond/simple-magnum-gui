fae18f9bad840316c9719ee21fd135067ce2d8c8
kolbbond <kolbbond@gmail.com>
2024-04-17 14:01:56 -0400

    working implot

--- scripts/create_clangd.sh
+++ scripts/create_clangd.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # create a .clangd for this build
+
+# make a clangd file with our cwd
+printf "${IBlue}create a .clangd to this directory!${NC}\n"
+printf "${IYellow}$(pwd)\n${NC}"
+printf "CompileFlags:\n" > .clangd
+printf "\tAdd:\n" >> .clangd
+
+# change the pwd
+printf "\t- -I$(pwd)/imgui\n" >> .clangd
+printf "\t- -I$(pwd)/implot\n" >> .clangd
