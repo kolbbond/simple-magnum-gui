1bac72b1e496ee41c7766abe59c59b59e22b66d6
kolbbond <kolbbond@gmail.com>
2025-02-19 14:14:45 -0500

    start adding vcpkg

---
+++ cmake/x64-windows-local.cmake
@@ -0,0 +1,30 @@
+# custom toolchain file for x64-windows.-local
+set(TRIPLET_NAME x64-windows.-local)
+message(STATUS "vcpkg build with triplet: ${TRIPLET_NAME}")
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+#set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+#set(VCPKG_BUILD_TYPE release)
+
+# from vcpkg triplets x64-windows.cmake
+#set(VCPKG_TARGET_ARCHITECTURE x64)
+#set(VCPKG_CRT_LINKAGE dynamic)
+#set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+#set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -fsanitize=address")
+
+# set environment variables
+set(VCPKG_ENV_PASSTHROUGH HOME)
+set(USER_DIR $ENV{HOME})
+message("User Folder: ${CMAKE_CURRENT_LIST_DIR}")
+set(ENV{BLA_VENDOR} OpenBLAS)
+set(ENV{LD_LIBRARY_PATH} ${USER_DIR}/.local/vcpkg/x64-windows-local/lib)
+
+# environment variables set
+message(STATUS "Environment variables set:")
+message(STATUS "  USER_DIR: ${USER_DIR}")
+message(STATUS "  BLA_VENDOR: ${BLA_VENDOR}")
+message(STATUS "  LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}")
+
+
