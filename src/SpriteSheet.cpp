c819d8e41475ae5339e06fd3d786527bdadb94ee
kolbbond <kolbbond@gmail.com>
2026-06-27 16:56:05 -0400

    sprites: optional SpriteSheet::load via StbImageImporter (SMG_WITH_IMAGE_IMPORT)

--- src/SpriteSheet.cpp
+++ src/SpriteSheet.cpp
@@ -3,6 +3,14 @@
 #include <Magnum/GL/TextureFormat.h>
 #include <Magnum/ImageView.h>
 
+#ifdef SMG_WITH_IMAGE_IMPORT
+#    include <Corrade/Containers/Optional.h>
+#    include <Corrade/Containers/Pointer.h>
+#    include <Corrade/PluginManager/Manager.h>
+#    include <Magnum/Trade/AbstractImporter.h>
+#    include <Magnum/Trade/ImageData.h>
+#endif
+
 namespace smg {
 
 SpriteSheet::SpriteSheet(Magnum::GL::Texture2D&& tex, const SpriteGrid& grid) : _texture{ std::move(tex) }, _grid{ grid } {}
@@ -29,5 +37,16 @@ ShSpriteSheetPr SpriteSheet::from_pixels(Corrade::Containers::ArrayView<const ch
         .setSubImage(0, {}, Magnum::ImageView2D{ format, size, pixels });
     return from_texture(std::move(tex), cols, rows);
 }
+
+#ifdef SMG_WITH_IMAGE_IMPORT
+ShSpriteSheetPr SpriteSheet::load(const char* path, int cols, int rows) {
+    Corrade::PluginManager::Manager<Magnum::Trade::AbstractImporter> manager{ SMG_IMPORTER_DIR };
+    Corrade::Containers::Pointer<Magnum::Trade::AbstractImporter> importer = manager.loadAndInstantiate("AnyImageImporter");
+    if(!importer || !importer->openFile(path)) return nullptr;
+    const Corrade::Containers::Optional<Magnum::Trade::ImageData2D> image = importer->image2D(0);
+    if(!image) return nullptr;
+    return from_pixels(image->data(), image->size(), image->format(), cols, rows);
+}
+#endif
 
 } // namespace smg
